233b19d7e1b60da3f7807262fc4b8738915b16d3
Jeff Scheel <jjscheel68@gmail.com>
2025-09-18 15:51:48 -0400

    Update docusaurus.config.ts
    Signed-off-by: Jeff Scheel <jjscheel68@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
   url: 'https://developer.riscv.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/antora.riscv.org/',
+  //baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
